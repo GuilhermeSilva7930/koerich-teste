94ceeb92a9b68b5cee8d0f5a14752c64adccc671
= <guilhermeferreira7930@gmail.com>
2022-07-11 17:42:26 -0300

    ATIVADE-002

---
+++ frontend/src/pages/atividade002/index.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from "react"
+import list from "../../api/repositorio.json"
+import "./index.css"
+import { Link } from "react-router-dom"
+
+function Atividade002() {
+
+    const input = useRef()
+    let [listAtualizada, setListaAtualizada] = useState(list)
+
+    function atualizarTabela() {
+        let texto = input.current.value.toLowerCase()
+        setListaAtualizada(list.filter(produto => produto.produto.toLowerCase().includes(texto)))
+    }
+
+    return (
+        <main className="container atividade001">
+            <div className="pesquisa">
+                <Link to="/" className="link-voltar ms-3">Voltar</Link>
+                <input onChange={atualizarTabela} ref={input} type="text" className="me-3" placeholder="Digite para filtrar..." />
+            </div>
+            <div className="scroll">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Nome do Produto</th>
+                            <th>Quantidade em estoque</th>
+                            <th>Peças com defeitos</th>
+                            <th>Peças para venda</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listAtualizada.map(produto => (
+                            <tr key={produto.key}>
+                                <td>{produto.produto}</td>
+                                <td>{produto.quantidade}</td>
+                                <td>{produto.defeito}</td>
+                                <td>{produto.quantidade - produto.defeito}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </main>
+    )
+}
+
+export default Atividade002
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import App from "../App";
 import Atividade001 from "../pages/atividade001";
+import Atividade002 from "../pages/atividade002";
 
 function Rotas() {
     return (
@@ -9,6 +10,7 @@ function Rotas() {
             <Routes>
                 <Route path="/" exact element={<App />} />
                 <Route path="/atividade001" exact element={<Atividade001 />} />
+                <Route path="/atividade002" exact element={<Atividade002 />} />
             </Routes>
         </BrowserRouter>
     )
